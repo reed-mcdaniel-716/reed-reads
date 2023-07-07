c0b59c434289b2672c6c711c237b3975b9e7c3d1
Reed McDaniel <reed.mcdaniel.716@gmail.com>
2023-07-07 08:13:57 -0400

    scaffolding react app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Reed's Reads Coming Soon...</h1>
+    </div>
+  );
+}
+
+export default App;
