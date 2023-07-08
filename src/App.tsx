a2e9340fa15def97299ec572959098add83945fd
Reed McDaniel <reed.mcdaniel.716@gmail.com>
2023-07-08 09:52:11 -0400

    starting on navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,43 @@
 import React from "react";
 import "./App.css";
+import { extendTheme, ChakraProvider } from "@chakra-ui/react";
+import NavBar from "./components/NavBar/NavBar";
+import "@fontsource/vollkorn/400.css";
+import "@fontsource/vollkorn/500.css";
+import "@fontsource/vollkorn/600.css";
+import "@fontsource/vollkorn/700.css";
+import "@fontsource/vollkorn/800.css";
+import "@fontsource/vollkorn/900.css";
+
+const colors = {
+  brand: {
+    seasalt: "#F8F8F8ff",
+    brown_sugar: "#B25F3Bff",
+    ash_gray: "#B8BBADff",
+    chamoisee: "#93765Cff",
+    viridian: "#1D7E62ff",
+    black_olive: "#383526ff",
+    gamboge: "#E9A141ff",
+  },
+};
+
+const fonts = {
+  heading: `'Vollkorn', sans-serif`,
+  body: `'Vollkorn', sans-serif`,
+};
+
+const theme = extendTheme({
+  colors: { ...colors },
+  fonts: { ...fonts },
+});
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Reed's Reads Coming Soon...</h1>
-    </div>
+    <ChakraProvider theme={theme}>
+      <div>
+        <NavBar />
+      </div>
+    </ChakraProvider>
   );
 }
 
---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Box, Flex, Image, Input, Spacer, Text } from "@chakra-ui/react";
+
+function NavBar() {
+  return (
+    <Flex
+      minWidth='max-content'
+      alignItems='center'
+      bg='brand.black_olive'
+      color='brand.ash_gray'
+      padding='0.75rem'
+    >
+      <Box>
+        <Image
+          boxSize='2rem'
+          objectFit='cover'
+          alt="Reed's Reads Logo"
+          src='/android-chrome-192x192.png'
+        />
+      </Box>
+      <Spacer />
+      <Text fontSize='2rem'>Reed's Reads</Text>
+      <Spacer />
+      <Input
+        width='20rem'
+        placeholder='Search for your next great adventure...'
+        backgroundColor='brand.ash_gray'
+        color='brand.black_olive'
+        borderColor='brand.black_olive'
+        _placeholder={{ color: "brand.black_olive" }}
+      />
+    </Flex>
+  );
+}
+
+export default NavBar;
